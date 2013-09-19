50cdd5bee69e7c0a6359fe04ec019cb02e62943e
Black.Princess <black.princess.w@gmail.com>
2013-09-20 02:14:56 +0900

    transplanted js to coffee

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,202 @@
 (function() {
-  window.drawColorWheel = function(paper, colors) {
+  var Analogous, Complementary, draw12ColorWheel, drawColorWheel, drawHexard, drawTriad, getOriginColor, resultRender;
+
+  $(function() {
+    /*
+    input support
+    */
+
+    var raphael;
+    $("#base_color_hex").focus(function() {
+      return $("#type_hex").click();
+    });
+    $("#base_color_r").focus(function() {
+      return $("#type_rgb").click();
+    });
+    $("#base_color_h").focus(function() {
+      return $("#type_hsv").click();
+    });
+    $(".base_color_255").change(function() {
+      var val;
+      val = ('' + $(this).val()).toFloatOrZero() % 256;
+      return $(this).val(val);
+    });
+    $(".base_color_359").change(function() {
+      var val;
+      val = ('' + $(this).val()).toFloatOrZero() % 360;
+      return $(this).val(val);
+    });
+    /*
+    event ?
+    */
+
+    $("#execute").click(function(ev) {
+      resultRender();
+      return false;
+    });
+    /*
+    # svg
+    */
+
+    raphael = function(id) {
+      return Raphael(id, $("#" + id).width(), $("#" + id).height());
+    };
+    return window.papers = {
+      triadWheel: 　raphael("triad_wheel_svg"),
+      hexardWheel: raphael("hexard_wheel_svg"),
+      colorWheel: raphael("color_wheel_svg")
+    };
+  });
+
+  /*
+  # render all blocks
+  */
+
+
+  resultRender = function() {
+    var origin;
+    origin = getOriginColor();
+    Complementary.render(origin);
+    drawTriad(origin);
+    drawHexard(origin);
+    Analogous.render(origin);
+    return draw12ColorWheel(origin);
+  };
+
+  /*
+  # get origin color from inputs
+  */
+
+
+  getOriginColor = function() {
+    var type;
+    type = $("input[name=type]:checked").val().toLowerCase();
+    switch (type) {
+      case "hex":
+        return princessJs.Color.createFromHexCode($("#base_color_hex").val());
+      case "rgb":
+        return princessJs.Color.create($("#base_color_r").val().toInt(), $("#base_color_g").val().toInt(), $("#base_color_b").val().toInt());
+      case "hsv":
+        return princessJs.Color.createFromHSV($("#base_color_h").val().toInt(), $("#base_color_s").val().toInt(), $("#base_color_v").val().toInt());
+    }
+  };
+
+  /*
+  #
+  */
+
+
+  Complementary = (function() {
+    var gradationRate;
+
+    function Complementary() {}
+
+    Complementary.base = function(origin) {
+      return origin.addHue(180);
+    };
+
+    gradationRate = -20;
+
+    Complementary.render = function(origin) {
+      var base, originHexCode;
+      base = this.base(origin);
+      originHexCode = origin.toCssHexCode();
+      $("#complementary .origin").css("background-color", originHexCode);
+      $("#complementary .origin").text(originHexCode);
+      $("#complementary .origin").each(function(index) {
+        var complementaryHexCode;
+        complementaryHexCode = base.addSaturation(gradationRate * index).toCssHexCode();
+        $(this).css("color", complementaryHexCode);
+        return $(this).text(complementaryHexCode);
+      });
+      $("#complementary .complementary").css("color", originHexCode);
+      return $("#complementary .complementary").each(function(index) {
+        var complementaryHexCode;
+        complementaryHexCode = base.addSaturation(gradationRate * index).toCssHexCode();
+        $(this).css("background-color", complementaryHexCode);
+        return $(this).text(complementaryHexCode);
+      });
+    };
+
+    return Complementary;
+
+  })();
+
+  /*
+  # Analogous Block
+  */
+
+
+  Analogous = (function() {
+    function Analogous() {}
+
+    Analogous.colors = function(origin) {
+      return [-3, -2, -1, 0, 1, 2, 3].map(function(i) {
+        return origin.addHue(i * 10);
+      });
+    };
+
+    Analogous.render = function(origin) {
+      $("#analogous .origin").css("background-color", origin.toCssHexCode());
+      $("#analogous .origin").text(origin.toCssHexCode());
+      return this.colors(origin).zip($("#analogous").find(".analogous")).forEach(function(a) {
+        var $analogous, color;
+        color = a[0];
+        $analogous = $(a[1]);
+        $analogous.css("background-color", color.toCssHexCode());
+        return $analogous.text(color.toCssHexCode());
+      });
+    };
+
+    return Analogous;
+
+  })();
+
+  /*
+  # draw Triad color wheel
+  */
+
+
+  drawTriad = function(origin) {
+    var colors;
+    colors = [0, 1, 2].map(function(i) {
+      return origin.addHue(i * 120);
+    });
+    return drawColorWheel(papers.triadWheel, colors);
+  };
+
+  /*
+  # draw Hexard color wheel
+  */
+
+
+  drawHexard = function(origin) {
+    var colors;
+    colors = [0, 1, 2, 3, 4, 5].map(function(i) {
+      return origin.addHue(i * 60);
+    });
+    return drawColorWheel(papers.hexardWheel, colors);
+  };
+
+  /*
+  # draw color wheel
+  */
+
+
+  draw12ColorWheel = function(origin) {
+    var colors;
+    colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(function(i) {
+      return origin.addHue(i * 30);
+    });
+    return drawColorWheel(papers.colorWheel, colors);
+  };
+
+  /*
+  # draw color wheel
+  */
+
+
+  drawColorWheel = function(paper, colors) {
     var animationMs, center, color, diffRad, end, i, start, txt, _i, _len;
     paper.clear();
     center = {
@@ -46,6 +243,17 @@
       'fill': "#ffffff",
       'stroke-width': 0
     });
+  };
+
+  window.RADIAN = Math.PI / 180;
+
+  window.Raphael.fn.circleSector = function(cx, cy, r, startAngle, endAngle) {
+    var x1, x2, y1, y2;
+    x1 = cx + r * Math.cos(-startAngle * RADIAN);
+    x2 = cx + r * Math.cos(-endAngle * RADIAN);
+    y1 = cy + r * Math.sin(-startAngle * RADIAN);
+    y2 = cy + r * Math.sin(-endAngle * RADIAN);
+    return this.path(["M", cx, cy, "L", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2, "z"]);
   };
 
 }).call(this);
--- src/index.coffee
+++ src/index.coffee
@@ -1,7 +1,7 @@
-###
-input support
-###
 $ ->
+  ###
+  input support
+  ###
   $("#base_color_hex").focus ->
     $("#type_hex").click()  # HACK
 
@@ -21,10 +21,124 @@ $ ->
     val = ('' + $(@).val()).toFloatOrZero() % 360;
     $(@).val(val);
 
+  ###
+  event ?
+  ###
+  $("#execute").click (ev) ->
+    resultRender()
+    false
+
+  ###
+  # svg 
+  ###
+  raphael = (id) -> Raphael(id, $("#" + id).width(), $("#" + id).height());
+  window.papers = 
+    triadWheel:　raphael("triad_wheel_svg")
+    hexardWheel: raphael("hexard_wheel_svg")
+    colorWheel: raphael("color_wheel_svg")
+
 ###
-# draw color whell
+# render all blocks
 ###
-window.drawColorWheel = (paper, colors) -> 
+resultRender = ->
+  origin = getOriginColor()
+  Complementary.render(origin)
+  drawTriad(origin)
+  drawHexard(origin)
+  Analogous.render(origin)
+  draw12ColorWheel(origin)
+
+###
+# get origin color from inputs
+###
+getOriginColor = ->
+  type = $("input[name=type]:checked").val().toLowerCase()
+  switch type
+    when "hex"
+      princessJs.Color.createFromHexCode($("#base_color_hex").val())
+    when "rgb"
+      princessJs.Color.create(
+        $("#base_color_r").val().toInt()
+        $("#base_color_g").val().toInt()
+        $("#base_color_b").val().toInt())
+    when "hsv"
+      princessJs.Color.createFromHSV(
+        $("#base_color_h").val().toInt()
+        $("#base_color_s").val().toInt() 
+        $("#base_color_v").val().toInt())
+###
+#
+###
+class Complementary
+  @base = (origin) -> origin.addHue 180
+  gradationRate = -20
+
+  @render = (origin) ->
+    base = @base(origin)
+    originHexCode = origin.toCssHexCode()
+    $("#complementary .origin").css("background-color", originHexCode);
+    $("#complementary .origin").text(originHexCode);
+    
+    $("#complementary .origin").each (index) ->
+      complementaryHexCode = base.addSaturation(gradationRate * index).toCssHexCode()
+      $(this).css("color", complementaryHexCode)
+      $(this).text(complementaryHexCode)
+
+    $("#complementary .complementary").css("color", originHexCode);
+    $("#complementary .complementary").each (index) -> 
+      complementaryHexCode = base.addSaturation(gradationRate * index).toCssHexCode()
+      $(this).css("background-color", complementaryHexCode)
+      $(this).text(complementaryHexCode)
+    
+
+###
+# Analogous Block
+###
+class Analogous
+  @colors = (origin) -> 
+    [-3..3].map (i) ->
+      origin.addHue i * 10
+
+  @render = (origin) ->
+    $("#analogous .origin").css("background-color", origin.toCssHexCode())
+    $("#analogous .origin").text(origin.toCssHexCode())
+    @colors(origin).zip($("#analogous").find(".analogous"))
+      .forEach (a) ->
+        color = a[0]
+        $analogous = $(a[1])
+        $analogous.css("background-color", color.toCssHexCode())
+        $analogous.text(color.toCssHexCode())
+    
+
+
+###
+# draw Triad color wheel
+###
+drawTriad = (origin) ->
+  colors = [0..2].map (i) -> 
+    origin.addHue i * 120
+  drawColorWheel(papers.triadWheel, colors)
+
+###
+# draw Hexard color wheel
+###
+drawHexard = (origin) ->
+  colors = [0..5].map (i) -> 
+    origin.addHue i * 60
+  drawColorWheel(papers.hexardWheel, colors)
+
+###
+# draw color wheel
+###
+draw12ColorWheel = (origin) ->
+  colors = [0..11].map (i) ->
+    origin.addHue i * 30
+  drawColorWheel(papers.colorWheel, colors)
+  
+###
+# draw color wheel
+###
+drawColorWheel = (paper, colors) -> 
   paper.clear()
   center = 
     x: paper.width / 2
@@ -66,3 +180,18 @@ window.drawColorWheel = (paper, colors) ->
     .attr(
       'fill':"#ffffff"
       'stroke-width':0)
+
+###############################
+# Raphael
+###############################
+window.RADIAN = Math.PI / 180;
+window.Raphael.fn.circleSector = (cx, cy, r, startAngle, endAngle) ->
+  x1 = cx + r * Math.cos(-startAngle * RADIAN)
+  x2 = cx + r * Math.cos(-endAngle * RADIAN)
+  y1 = cy + r * Math.sin(-startAngle * RADIAN)
+  y2 = cy + r * Math.sin(-endAngle * RADIAN)
+  this.path ["M", cx, cy
+    "L", x1, y1
+    "A", r, r
+    0, +(endAngle - startAngle > 180), 0
+    x2, y2, "z"]
